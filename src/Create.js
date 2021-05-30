0645abe23296d3132fcaac577cda8f8e2f57b699
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-05-30 23:13:33 +0200

    Submit Events

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,41 @@
+import { useState } from "react";
+
 const Create = () => {
-  return <h1>on create</h1>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mohamed");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
+  return (
+    <div className="create">
+      <h2>Add a new blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <label>body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mohamed">mohamed</option>
+          <option value="magdy">magdy</option>
+        </select>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
