a07c7e2de18c44389f3994196c5e4dc7eb863716
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-05-31 03:58:29 +0200

    delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,19 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const {
     data: blog,
     bending,
     error,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => history.push("/"));
+  };
   return (
     <div className="blog-details">
       {error && <h2>{error}</h2>}
@@ -17,6 +23,7 @@ const BlogDetails = () => {
           <h1>{blog.title}</h1>
           <p>written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
