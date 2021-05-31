6145af248fc76932f1c58ea450a860c03c713606
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-05-31 04:07:30 +0200

    not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Create from "./Create";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -14,11 +15,14 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/create">
+            <Route path="/create">
               <Create />
             </Route>
-            <Route exact path="/BlogDetails/:id">
+            <Route path="/BlogDetails/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
